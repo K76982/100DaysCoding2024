53735232786355b80798460493bb84d76a05e053
K76982 <dil803811@gmail.com>
2024-09-04 14:07:38 +0700

    Days 3
    system.out.println

---
+++ Days3.java
@@ -0,0 +1,11 @@
+package days.pkg3;
+
+public class Days3 {
+
+        public static void main(String[] args) {
+            System.out.println("Muhammad Aydil");
+            System.out.println("Informatika");
+            System.out.println("Umur 18");
+    }
+    
+}
