79803b7e7d0db24a832ebc615b7a15abd15e21cb
K76982 <dil803811@gmail.com>
2024-09-05 21:33:49 +0700

    Days4

---
+++ Days4.java
@@ -0,0 +1,14 @@
+package days4;
+
+public class Days4 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama : Muhammad Aydil\n");
+        System.out.print("Tb : 161 cm\n");
+        System.out.print("Umur : 18\n");
+       
+        
+          System.out.printf("Nama : %s%nUmur: %d Tahun%nTinggi Badan: %dcm%nBerat Badan: %dkg%n"," Muhammad Aydil", 18,161,50);
+    }
+    
+}
