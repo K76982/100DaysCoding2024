53e3b9fd9787223ed88bd9efc3aa5950c9b24fa2
K76982 <dil803811@gmail.com>
2024-09-05 15:08:18 +0700

    Days4
    system print dan system printf

---
+++ Days4.java
@@ -0,0 +1,14 @@
+package days4;
+
+public class Days4 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama");
+        System.out.print("Nim");
+        System.out.print("Fakultas");
+        System.out.printf("Muhammad Aydil");
+        System.out.printf("D0224028");
+        System.out.printf("Teknik");
+    }
+    
+}
