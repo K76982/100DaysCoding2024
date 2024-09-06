f45d0dc6021396cbcbd063634415c3ea4ce17746
K76982 <dil803811@gmail.com>
2024-09-06 15:35:31 +0700

    Days5
    komentar

---
+++ Days5.java
@@ -0,0 +1,20 @@
+package days.pkg5;
+
+public class Days5 {
+
+   
+    public static void main(String[] args) {
+        //Ini komentar
+        System.out.println("Nama : Aydil");
+        System.out.println("Fakultas : Teknik");
+        System.out.println("Prodi : Informatika");// Ini komentar 2
+        
+        /*
+        
+        Ini komentar 3
+        Ini komentar 4
+        
+        */
+    }
+    
+}
