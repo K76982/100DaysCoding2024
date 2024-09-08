7d3666fdeb08ef3d171bb79385caec6f9a8dc7bc
K76982 <dil803811@gmail.com>
2024-09-08 19:57:05 +0700

    Days 7

---
+++ Days7.java
@@ -0,0 +1,11 @@
+package days.pkg7;
+
+public class Days7 {
+
+     public static void main(String[] args) {
+        // Membuat Inisialisasi Variabel
+        String nama, us
+        Nama :
+    }
+    
+}
