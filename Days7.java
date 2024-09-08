6815eeb32451427449333bad5e31ce0fe0126946
K76982 <dil803811@gmail.com>
2024-09-08 20:12:00 +0700

    Days 7

---
+++ Days7.java
@@ -0,0 +1,25 @@
+package days.pkg7;
+
+public class Days7 {
+
+     public static void main(String[] args) {
+        // menginisialisasi variabel
+     
+        /*
+        String nama, usia, alamat;
+        
+        nama   = "Muhammad Aydil";
+        usia   = "18 tahun";
+        alamat = "galeso";
+        
+        */
+        //==========================
+         System.out.println("nama   : Muhammad Aydil");
+         System.out.println("usia   : 18 tahun");
+         System.out.println("alamat : galeso");
+        
+        
+       
+    }
+    
+}
