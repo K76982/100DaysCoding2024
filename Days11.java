504194897d62b5cf238ba8907492df5dca9edcac
K76982 <dil803811@gmail.com>
2024-09-12 21:07:53 +0700

    Days11

---
+++ Days11.java
@@ -0,0 +1,17 @@
+package days.pkg11;
+
+
+public class Days11 {
+
+    
+    public static void main(String[] args) {
+       // Tipe data short memiliki range angka dari -1243
+       short angka = -1243;
+       short angka1 = 2453;
+               
+        System.out.println("Minimum :" + angka);
+        System.out.println("Minimum :" + angka1);
+        
+    }
+    
+}
