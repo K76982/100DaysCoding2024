7e375bf53606c1c703769b035eb2f20b88d0f5ed
K76982 <dil803811@gmail.com>
2024-09-13 16:55:38 +0700

    Days12

---
+++ Days12.java
@@ -0,0 +1,18 @@
+package days.pkg12;
+
+public class Days12 {
+
+   
+    public static void main(String[] args) {
+        //Hasil tipe data int:
+        // tipe data int adalah tipe data primitif yang memiliki range dari -18 sampa 78
+     int age  = -18;
+     int year = 78;
+     
+        
+        System.out.println("Umur:" + age);
+        System.out.println("Tahun: " + year);
+        
+    }
+    
+}
